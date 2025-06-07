5b5270def9b7c9551f0c20a0935c019f072d55e0
Matthew Brown <mail.mattbrown@gmail.com>
2025-06-07 18:29:20 +0100

    feat: create and test place command

---
+++ src/commands/commands.test.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect } from 'vitest'
+import { place, type Config } from './commands'
+
+describe('place', () => {
+  it('should return the specified config if inside the bounds', () => {
+    const validConfig: Config = { x: 1, y: 2, f: 'NORTH' }
+    expect(place(validConfig)).toEqual(validConfig)
+  })
+
+  it('should throw an error when the config is out of bounds in the x direction', () => {
+    const invalidConfig: Config = { x: 5, y: 2, f: 'NORTH' }
+    expect(() => place(invalidConfig)).toThrow()
+  })
+
+  it('should throw an error when the config is out of bounds in the y direction', () => {
+    const invalidConfig: Config = { x: 3, y: 5, f: 'NORTH' }
+    expect(() => place(invalidConfig)).toThrow()
+  })
+
+  it('should thrown an error when the config contains a non-integer x coordinate', () => {
+    const invalidConfig: Config = { x: 3.2, y: 4, f: 'NORTH' }
+    expect(() => place(invalidConfig)).toThrow()
+  })
+
+  it('should thrown an error when the config contains a non-integer y coordinate', () => {
+    const invalidConfig: Config = { x: 3.2, y: 4, f: 'NORTH' }
+    expect(() => place(invalidConfig)).toThrow()
+  })
+
+  it('should thrown an error when the config contains a negative x coordinate', () => {
+    const invalidConfig: Config = { x: -1, y: 4, f: 'NORTH' }
+    expect(() => place(invalidConfig)).toThrow()
+  })
+
+  it('should thrown an error when the config contains a negative y coordinate', () => {
+    const invalidConfig: Config = { x: 1, y: -4, f: 'NORTH' }
+    expect(() => place(invalidConfig)).toThrow()
+  })
+})
---
+++ src/commands/commands.ts
@@ -0,0 +1,41 @@
+export type Orientation = 'NORTH' | 'EAST' | 'SOUTH' | 'WEST'
+
+export interface Config {
+  x: number
+  y: number
+  f: Orientation
+}
+
+const minBound = 0
+const maxBound = 4
+
+class NonIntegerCoordinateError extends Error {
+  constructor() {
+    super('NonIntegerCoordinateError')
+  }
+}
+
+class OutOfBoundCoordinateError extends Error {
+  constructor() {
+    super('OutOfBoundCoordinateError')
+  }
+}
+
+const validateConfig = (config: Config) => {
+  if (!Number.isInteger(config.x) || !Number.isInteger(config.y)) {
+    throw new NonIntegerCoordinateError()
+  }
+  if (
+    config.x < minBound ||
+    config.y < minBound ||
+    config.x > maxBound ||
+    config.y > maxBound
+  ) {
+    throw new OutOfBoundCoordinateError()
+  }
+}
+
+export const place = (config: Config): Config => {
+  validateConfig(config)
+  return config
+}
